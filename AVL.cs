76e6089f82b0718e946d3b81281d50068bfe474a
atul <atulmirajkar@gmail.com>
2022-06-14 23:17:56 -0500

    first commit

---
+++ AVL.cs
@@ -0,0 +1,156 @@
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System;
+namespace AVLTree
+{
+    public class AVL{
+
+        public class Node{
+            public Node left{get; set;}
+            public Node right{get; set;} 
+            public int value{get; set;}
+            
+            public int height{get; set;}
+            public int bf { get; set; }
+
+            public Node(int value){
+                this.value = value;
+                height = 1;
+            }
+        }
+
+        private Node root{get; set;}
+
+        //todo implement size
+        private int size{get; set;}
+
+        private void updateHeight(Node node){
+            if(node == null)
+                return;
+            
+            int leftHeight = node.left != null ? node.left.height : 0;
+            int rightHeight = node.right != null ? node.right.height : 0;
+            node.height = 1+ Math.Max(leftHeight,rightHeight);
+
+            node.bf = leftHeight - rightHeight;   
+        }
+
+        private Node rotateRight(Node node){
+            if(node == null)
+                return null;
+            
+            Node pivot = node.left;
+            //if pivot itself is null dont do anything
+            if(pivot == null)
+                return node;
+
+            Node temp = pivot.right;
+            pivot.right = node;
+            node.left = temp;
+            
+            //update heights
+            //update input node's height first because it goes below the pivot node
+            updateHeight(node);
+            updateHeight(pivot);
+            return pivot;
+        }
+
+        private Node rotateLeft(Node node){
+            if(node == null)
+                return null;
+            
+            Node pivot = node.right;
+            //if pivot itself is null dont do anything
+            if(pivot == null)
+                return node;
+            
+            Node temp = pivot.left;
+            pivot.left = node;
+            node.right = temp;
+            
+
+            //update heights
+            updateHeight(node);
+            updateHeight(pivot);
+
+            return pivot;
+        }
+
+        private Node rebalance(Node node, int value){
+            if(node == null)
+                return null;
+
+            int balance = node.bf;
+            //left - left case
+            if(balance > 1){
+                if(value < node.left.value)
+                    return rotateRight(node);
+                
+                //left - right case
+                if(value > node.left.value){
+                    node.left = rotateLeft(node.left);
+                    return rotateRight(node);
+                }
+            }else if(balance < 1){
+                //right right case
+                if(value > node.right.value)
+                    return rotateLeft(node);
+
+                //right left case
+                if(value < node.right.value){
+                    node.right = rotateRight(node.right);
+                    return rotateLeft(node);
+                }
+            }
+            return null;
+        }
+        public Node Insert(int value){
+            root = insert(value, root);
+            return root;
+        }
+
+        private Node insert(int value, Node node){
+            //exit condition
+            if(node == null){
+                return new Node(value);
+            }
+
+            //perform normal bst insertion
+            //dont allow duplicates
+            if(node.value == value){
+                return node;
+            }
+            if(value < node.value)
+            {
+                node.left = insert(value, node.left);
+            }else{
+                node.right = insert(value, node.right);
+            }
+
+            //update height as we unwind the stack
+            updateHeight(node);
+
+            //check whether bf within limits
+            if(node.bf < -1 ||  node.bf > 1){
+                return rebalance(node, value);
+            }
+            
+            return node;
+        }
+
+        public void PreOrder(){
+            preOrder(root);
+        }
+
+        private void preOrder(Node node){
+            //exit condition 
+            if(node == null)
+                return;
+            
+            Console.WriteLine(node.value);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+}
---
+++ behavior.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace AVLTree
+{
+    class behavior
+    {
+        static void Main(string[] args)
+        {
+            AVL tree = new AVL();
+            tree.Insert(10);
+            tree.Insert(5);
+            tree.Insert(7);
+            tree.PreOrder();
+        }
+    }
+}
