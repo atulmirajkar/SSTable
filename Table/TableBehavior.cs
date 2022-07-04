6b8e5fb23c2d0f32ea0734cd8a02bddc3ab1d854
atulmirajkar <atulmirajkar@gmail.com>
2022-07-04 18:27:42 -0500

    add Table project:

---
+++ Table/TableBehavior.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Table// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
