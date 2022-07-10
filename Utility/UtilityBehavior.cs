5bd644d4e61b1ac88a943befc2521db2fbff5b6c
atulmirajkar <atulmirajkar@gmail.com>
2022-07-10 09:40:52 -0500

    unit case

--- Utility/UtilityBehavior.cs
+++ Utility/UtilityBehavior.cs
@@ -8,7 +8,8 @@ namespace Utility
         public static void Main()
         {
             DynamicClass dc = new DynamicClass("MyAssemblyName","MyClassName");
-            dc.AddField("MyField",typeof(int));
+            dc.AddField("MyIntField",typeof(int));
+            dc.AddField("MyStringField",typeof(string));
             Type? myType = dc.CreateType();
             if(myType == null){
                 Console.WriteLine("Could not create type");
@@ -19,10 +20,14 @@ namespace Utility
                 Console.WriteLine("Could not create object");
                 return;
             }
-            PropertyInfo? pi = myType.GetProperty("MyField");
+            PropertyInfo? pi = myType.GetProperty("MyIntField");
             pi?.SetValue(obj,10);
+            
+            PropertyInfo? piStr = myType.GetProperty("MyStringField");
+            piStr?.SetValue(obj,"someValue");
 
             Console.WriteLine(pi?.GetValue(obj));
+            Console.WriteLine(piStr?.GetValue(obj));
         }
 
     }
